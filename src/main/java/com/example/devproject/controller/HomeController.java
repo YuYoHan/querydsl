5e342dbdc77cd4ca2d9b82e87f1ab83544652d81
YuYoHan <zxzz8014@naver.com>
2023-05-09 11:21:05 +0900

    [Feat] Home 기능 구현

--- src/main/java/com/example/devproject/controller/HomeController.java
+++ src/main/java/com/example/devproject/controller/HomeController.java
@@ -1,12 +1,20 @@
 package com.example.devproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 (E)a h시 m분 s초");
+        String formattedNow = now.format(formatter);
+        model.addAttribute("serverTime", formattedNow);
         return "home";
     }
 }

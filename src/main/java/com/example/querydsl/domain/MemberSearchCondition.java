c28b4ba5182dfe9c30bd6a0edda4d32e97fb3d56
YuYoHan <zxzz8014@naver.com>
2024-01-06 17:37:23 +0900

    [Fix] 동적 쿼리 문제 해결

--- src/main/java/com/example/querydsl/domain/MemberSearchCondition.java
+++ src/main/java/com/example/querydsl/domain/MemberSearchCondition.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @ToString
 @Getter
-@NoArgsConstructor
 public class MemberSearchCondition {
     // 회원명, 팀명, 나이(ageGoe, ageLoe)
 
@@ -20,20 +19,17 @@ public class MemberSearchCondition {
     // 나이가 작거나 같거나
     private Integer ageLoe;
     private String orderBy;
-    private Sort sort;
 
     @Builder
     public MemberSearchCondition(String userName,
                                  String teamName,
                                  Integer ageGoe,
                                  Integer ageLoe,
-                                 String orderBy,
-                                 Sort sort) {
+                                 String orderBy) {
         this.userName = userName;
         this.teamName = teamName;
         this.ageGoe = ageGoe;
         this.ageLoe = ageLoe;
         this.orderBy = orderBy;
-        this.sort = sort;
     }
 }
--- src/main/java/com/example/querydsl/domain/Sort.java
+++
@@ -1,5 +0,0 @@
-package com.example.querydsl.domain;
-
-public enum Sort {
-    DESC, ASC
-}
